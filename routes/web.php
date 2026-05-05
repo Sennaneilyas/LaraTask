02b6c0192325a9829db3c333e726ef39773c4ea5
Sennane Ilyas <sennaneilyas26@gmail.com>
2026-05-05 09:02:35 +0100

    Routes configured.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('account')->name('account.')->middleware('auth')->group(function () {
+    Route::get('profile', [UserController::class, 'profile'])->name('profile');
+    Route::put('password-change', [UserController::class, 'passwordChange'])->name('password.change');
+});
+
+Route::prefix('projects')->name('projects.')->group(function () {
+    Route::resource('/', ProjectController::class)->middleware('auth');
+    Route::prefix('{project}')->group(function () {
+        Route::resource('tasks', TaskController::class)->shallow();
+    });
 });
