758c841c063c6070ded9451201fe722cf708c7f7
Sennane Ilyas <sennaneilyas26@gmail.com>
2026-05-07 18:06:28 +0100

    reoutes updated routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,35 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('layouts.app');
+})->name('home');
 
-Route::prefix('account')->name('account.')->middleware('auth')->group(function () {
-    Route::get('profile', [UserController::class, 'profile'])->name('profile');
-    Route::put('password-change', [UserController::class, 'passwordChange'])->name('password.change');
+Route::middleware('guest')->group(function () {
+    Route::view('/login', 'auth.login')->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::view('/register', 'auth.register')->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
 });
 
 Route::middleware('auth')->group(function () {
+    Route::get('/account/profile', [UserController::class, 'profile'])
+        ->name('profile.show');
+
+    Route::get('/account/password', [UserController::class, 'editPassword'])
+        ->name('password.edit');
+    Route::put('/account/password', [UserController::class, 'updatePassword'])
+        ->name('password.update');
+
+    Route::post('/logout', [AuthController::class, 'logout'])
+        ->name('logout');
+
     Route::resource('projects', ProjectController::class);
     Route::resource('projects.tasks', TaskController::class)->scoped();
 });
