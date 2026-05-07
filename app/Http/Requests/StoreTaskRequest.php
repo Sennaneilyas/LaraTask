ceefa0aed8a3392f2b7572ebfa12d020e097602b
Sennane Ilyas <sennaneilyas26@gmail.com>
2026-05-07 13:25:10 +0100

    store task request added.

---
+++ app/Http/Requests/StoreTaskRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Task;
+use Illuminate\Validation\Rule;
+
+class StoreTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()?->can('create', Task::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'priority' => ['required', Rule::in(['high', 'medium', 'low'])],
+            'project_id' => [
+                'required',
+                Rule::exists('projects', 'id')
+                    ->where('status', 'active')
+            ],
+            'due_date' => ['nullable', 'date', 'after:today']
+        ];
+    }
+}
